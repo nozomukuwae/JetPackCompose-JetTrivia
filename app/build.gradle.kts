4eb29b458d0ff5fe2810a08ca734d281ecbca2f0
Nozomu Kuwae <nozomukuwae@gmail.com>
2023-09-02 16:26:55 +0900

    Set up Hilt & Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
     }
     buildFeatures {
         compose = true
---
+++ app/src/main/java/com/example/jettrivia/di/AppModule.kt
@@ -0,0 +1,23 @@
+package com.example.jettrivia.di
+
+import com.example.jettrivia.network.QuestionApi
+import com.example.jettrivia.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideQuestionApi(): QuestionApi = Retrofit.Builder()
+        .baseUrl(Constants.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(QuestionApi::class.java)
+}
